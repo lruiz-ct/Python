170161ad6be740794f0536fbdfd9fdcbf04845e4
Luis Ruiz <luis@MacBook-Pro-de-Clara.local>
2021-10-14 19:02:25 -0700

    Added Session 3

---
+++ Session3/exercises/exercise1.py
@@ -0,0 +1,6 @@
+value = 0
+
+while value <=100:
+    temp = int(input("Write a number:"))
+    value += temp
+    print (f"Current value:{value}")
---
+++ Session3/simple_loop.py
@@ -0,0 +1,13 @@
+for num in [8, 1, 2, 3, 4]:
+    print(num)
+
+print()
+
+for car in ["Ford", "Volvo", "BMW"]:
+    print (car)
+
+print()
+
+for i in range(5):
+    print (i)
+
